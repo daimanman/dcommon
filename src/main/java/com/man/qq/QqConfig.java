ff7b7207eba1176a630dd41716cc793bce4915aa
daixiaoman <2821224626@qq.com>
2018-11-12 17:29:15 +0800

    ok

--- src/main/java/com/man/qq/QqConfig.java
+++ src/main/java/com/man/qq/QqConfig.java
@@ -2,10 +2,39 @@ package com.man.qq;
 
 public class QqConfig {
 
+	// shuoshuo
 	public static String QQ_EMOTINFO_URL = "https://h5.qzone.qq.com/proxy/domain/taotao.qq.com/cgi-bin/emotion_cgi_msglist_v6";
+
+	// 个人信息
 	public static String QQ_BASEINFO_URL = "https://h5.qzone.qq.com/proxy/domain/base.qzone.qq.com/cgi-bin/user/cgi_userinfo_get_all";
+
+	// msg
 	public static String QQ_MSGINFO_URL = "https://h5.qzone.qq.com/proxy/domain/m.qzone.qq.com/cgi-bin/new/get_msgb";
+
+	// photo
 	public static String QQ_PHOTOINFO_URL = "https://h5.qzone.qq.com/proxy/domain/alist.photo.qq.com/fcgi-bin/fcg_list_album_v3";
+
+	// image
 	public static String QQ_IMGINFO_URL = "https://h5.qzone.qq.com/proxy/domain/plist.photo.qzone.qq.com/fcgi-bin/cgi_list_photo";
+
+	// visit
 	public static String QQ_VISITINFO_URL = "https://h5.qzone.qq.com/proxy/domain/g.qzone.qq.com/cgi-bin/friendshow/cgi_get_visitor_simple";
+
+	//publish emot 
+	public static String QQ_SENT_EMOT_URL = "https://user.qzone.qq.com/proxy/domain/taotao.qzone.qq.com/cgi-bin/emotion_cgi_publish_v6";
+	// emot num
+	public static int DEFAULT_NUM = 30;
+
+	// msg num
+	public static int DEFAULT_MSG_NUM = 20;
+
+	// img num
+
+	public static int DEFAULT_IMG_NUM = 500;
+
+	public static int CODE_VERSION = 1;
+
+	public static int REPLYNUM = 100;
+
+	public static String ALLOW_ACCESS = "1";
 }
---
+++ src/main/java/com/man/utils/ReqParam.java
@@ -0,0 +1,16 @@
+package com.man.utils;
+
+import java.util.HashMap;
+
+public class ReqParam extends HashMap<String,Object>{
+
+	private static final long serialVersionUID = -4078948510474302368L;
+
+	public String getStr(String key) {
+		return ObjectUtil.toString(this.get(key),"");
+	}
+	
+	public int getInt(String key) {
+		return ObjectUtil.parseInt(this.get(key));
+	}
+}
